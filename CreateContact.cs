8be88597d70d937efe96dc9bc752c99ace51c38a
Harry <hareshwarp649>
2022-02-28 14:14:51 +0530

    Create contacts in Address

---
+++ CreateContact.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class CreateContact
+    {
+
+
+        static String firstName;
+        static String lastName;
+        static long phoneNumber;
+        static String email;
+        static String address;
+        static String city;
+        static String state;
+        static int zipCode;
+        public static List<String> addressBook = new List<String>();
+
+       
+        public static void ContactCreate()
+        {
+
+            NewContact(addressBook);
+        }
+
+
+        
+        public static void NewContact(List<string> addressBook)
+        {
+            int count = 0;
+            string contact;
+            char ch;
+            do
+            {
+                Console.WriteLine("\nPlease Enter Contact Details: \n");
+                ContactDeatils();
+                Console.WriteLine("\nContact Created.\n");
+
+                contact = firstName + " " + lastName + "\n " + phoneNumber.ToString() + " " + email + "\n " + address + " " + city + " " + state + " " + zipCode.ToString();
+                addressBook.Add(contact);
+                count++;
+
+                Console.WriteLine("Do you want to continue: y/n");
+                ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("\n-------------------------------------------------------------------------------------------");
+            }
+            while (ch == 'y');
+
+
+            Console.WriteLine("\n{0} contacts\n", count);
+
+            for (int i = 0; i < addressBook.Count; i++)
+            {
+                Console.WriteLine("{0}.  {1}", i + 1, addressBook[i]);
+            }
+            toContinue();
+        }
+
+       
+        public static void ContactDeatils()
+        {
+
+            Console.Write("First Name : ");
+            firstName = Console.ReadLine();
+            Console.Write("Last Name : ");
+            lastName = Console.ReadLine();
+            Console.Write("Phone Number : ");
+            phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Email : ");
+            email = Console.ReadLine(); ;
+            Console.Write("Address : ");
+            address = Console.ReadLine();
+            Console.Write("City : ");
+            city = Console.ReadLine();
+            Console.Write("State : ");
+            state = Console.ReadLine();
+            Console.Write("Zip : ");
+            zipCode = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+
+        public static void toContinue()
+        {
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------------------");
+            char ch;
+            Console.WriteLine("Do you want to continue: y/n");
+            ch = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("\n\n");
+            if (ch == 'y')
+            {
+                ContactCreate();
+            }
+            else
+            {
+                Environment.Exit(-1);
+            }
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------------------");
+        }
+
+
+
+
+    }
+}
