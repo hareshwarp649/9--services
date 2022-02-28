64a0ac6b102c8430bab7295a29943cd6b0280983
Harry <hareshwarp649>
2022-02-28 14:01:39 +0530

    First Program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CreatAddress.AddressCreate();
+            //  CreateContact.ContactCreate();
+          //  UpdateAddress.AddressUpdate();
+            DeleteAddress.AdressDelete();
+        }
+    }
+}
