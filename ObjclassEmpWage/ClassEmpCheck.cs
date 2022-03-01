d0f9e9abff0e78bddcab3d30cc71d76f2baf8916
Harry <hareshwarp649>
2022-03-01 11:11:17 +0530

    Employee Wage Computation prob

---
+++ ObjclassEmpWage/ClassEmpCheck.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeObj
+{
+    internal class ClassEmpCheck
+    {
+
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string company;
+        private int empRate;
+        private int numOfWorkingDay;
+        private int maxHrsMonth;
+        private int totalEmpWage;
+
+
+        public ClassEmpCheck(string company, int empRate, int numOfWorkingDay, int maxHrsMonth)
+        {
+           this.company = company;
+            this.empRate = empRate;
+            this.numOfWorkingDay = numOfWorkingDay;
+            this.maxHrsMonth = maxHrsMonth;
+        }
+       
+
+        
+        public void EmpClass()
+
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= maxHrsMonth && totalWorkingDays < numOfWorkingDay)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(1, 2);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+
+                }
+                totalEmpHrs += empHrs;
+
+                Console.WriteLine("Day:" + totalWorkingDays + "Employee Hourse :" + empHrs);
+
+
+            }
+            int totalEmpWage = totalEmpHrs * this.empRate;
+            Console.WriteLine("Total Emp Wage :" + company + "is:" +totalEmpWage);
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for Company:" + this.company + "is" + this.totalEmpWage;
+        }
+    }
+}
---
+++ ObjclassEmpWage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeObj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ClassEmpPre ravi = new ClassEmpPre(2);
+            //ravi.CheckEmp();
+
+
+            //EmpHourClass rem = new EmpHourClass(2, 3);
+            //rem.EmployeeDaily();
+
+
+
+
+
+            ClassEmpCheck mahindra = new ClassEmpCheck("Mahindra", 20, 2, 10);
+            ClassEmpCheck Jagauar = new ClassEmpCheck("Jagauar", 10, 4, 20);
+
+            mahindra.EmpClass();
+            Console.WriteLine(mahindra.toString());
+            Jagauar.EmpClass();
+            Console.WriteLine(Jagauar.toString());
+        }
+    }
+}
